b0008f58f13dda5c5741987605035f8db621fa0c
Edward Sumerfield <esumerfield@stackct.com>
2024-02-07 19:50:23 -0500

    complete day 7

---
+++ day-7/keep-an-eye/keep-an-eye.ino
@@ -0,0 +1,35 @@
+#include "Arduino.h"
+
+const byte PHOTORESISTOR_PIN = A0;  // Photoresistor analog pin
+
+const unsigned int BATTERY_CAPACITY = 50000;
+
+void setup() {
+  pinMode(PHOTORESISTOR_PIN, INPUT);
+
+  Serial.begin(9600);
+}
+
+unsigned int battery_level = 0;
+
+void loop() {
+
+  if (battery_level < BATTERY_CAPACITY) {
+    battery_level += analogRead(PHOTORESISTOR_PIN);
+    battery_level = min(battery_level, BATTERY_CAPACITY);
+
+    printBatteryChargeLevel();
+  }
+
+  delay(100);
+}
+
+void printBatteryChargeLevel() {
+  if (battery_level < BATTERY_CAPACITY) {
+    Serial.print(((double)battery_level / (double)BATTERY_CAPACITY) * 100);
+    Serial.println("%");
+  } else {
+    Serial.println("FULLY CHARGED");
+  }
+}
+
