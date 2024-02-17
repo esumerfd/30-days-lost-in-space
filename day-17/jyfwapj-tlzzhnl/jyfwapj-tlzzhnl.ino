c9ad8bf61ce31e00a87b94d9229d37338713d3be
Edward Sumerfield <esumerfield@stackct.com>
2024-02-17 13:10:29 -0500

    Complete day 16 decryption

--- day-17/jyfwapj-tlzzhnl/jyfwapj-tlzzhnl.ino
+++ day-17/jyfwapj-tlzzhnl/jyfwapj-tlzzhnl.ino
@@ -1,67 +1,48 @@
 /*
-* 29 Wtrl - Ehlm bg Litvx
-* Wtr 06 - A cryptic message
-*
-* Extkg fhkx tm ammil://bgoxgmk.bh/twoxgmnkx
-*
-* Vhgzktmnetmbhgl, by rhn'kx kxtwbgz mabl rhn'ox lnvvxllneer wxvhwxw max
-* mhi-lxvkxm ldxmva.  By rhn vhfibex tgw niehtw mabl ldxmva mh rhnk AXKH
-* pbma max ltfx pbkbgz tl hg Wtr 05 maxg rhn'ee lxx makxx "dxrl" wblietrxw
-* hoxk tgw hoxk pbma t uetgd uxyhkx max ybklm hgx bl wblietrxw.
-*
-* By mabl wxvhwxw ldxmva whxlg'm vhfibex ftdx lnkx rhn ikhixker bgvenwxw
-* gnfxkbv wbzbml bg rhnk wxvhwx!  By max gnfuxkl wbwg'm zxm wxvhwxw maxg
-* mabl wxvhwxw ldxmva phg'm vhfibex pbmahnm xkkhkl!
-*
-* Texq Xlvaxgtnxk
-* Wtobw Lvafbwm
-* Zkxz Ersxgzt
-*/
-
-/*
- * bgvenwx ohbw lxmni ehhi
  * include void setup loop
+ * pujsbkl cvpk zlabw svvw
  * 
- * abcdefghijklmnopqrstuvwxyz
- * vwx   b  e ghi  lmno   
- * shift right 5
+ * hijklmnopqrstuvwxyzabcdefg
+ * cde   i  l nop  stuv   
+ * zopma ypnoa 6 vy slma 20
  */
 
-// Xqiebvbmer bgvenwx Tkwnbgh.a
-#bgvenwx <Tkwnbgh.a>
+// Explicitly include Arduino.h
+#include <Arduino.h>
 
-// Bgvenwx MF0526 ebuktkr ybex
-#bgvenwx <MF0526Wblietr.a>
+// Include TM1637 library file
+#include <TM1637Display.h>
 
-// Fhwnex vhggxvmbhg ibgl (Wbzbmte Ibgl)
-vhglm urmx VED_IBG = 5;
-vhglm urmx WBH_IBG = 4;
+// Module connection pins (Digital Pins)
+const byte CLK_PIN = 6;
+const byte DIO_PIN = 5;
 
-// Tfhngm hy mbfx (bg fl) mh wxetr uxmpxxg hnk wxvbiaxkxw dxr otenxl
-vhglm nglbzgxw bgm WBLIETR_WXETR = 1999;
+// Amount of time (in ms) to delay between our deciphered key values
+const unsigned int DISPLAY_DELAY = 2000;
 
-MF0526Wblietr axkh_wblietr(VED_IBG, WBH_IBG);
+TM1637Display hero_display(CLK_PIN, DIO_PIN);
 
-vhglm nglbzgxw bgm OTEBWTMBHG_WTMT[] = {
- 'u',16,"t",9qtw1v,9u0999009099009099,"xz",'F'-'5','w',"mpxgmr-lxoxg",9436-5,
- 243,9q2,-16-52710,'j',471,9u0099009099000909,9321,9qwy2,31
+const unsigned int VALIDATION_DATA[] = {
+ 'b',27,"a",0xad2c,0b1000110100110100,"eg",'M'-'6','d',"twenty-seven",0547-6,
+ 354,0x3,-27-63821,'q',582,0b1100110100111010,0432,0xdf3,42
 };
 
-ohbw lxmni() {}
+void setup() {}
 
-ohbw ehhi() {
- axkh_wblietr.lxmUkbzamgxll(6);
+void loop() {
+ hero_display.setBrightness(7);
 
- axkh_wblietr.vextk();
- wxetr(WBLIETR_WXETR);
+ hero_display.clear();
+ delay(DISPLAY_DELAY);
 
- axkh_wblietr.lahpGnfuxkWxv(OTEBWTMBHG_WTMT['G'/904]);
- wxetr(WBLIETR_WXETR);
+ hero_display.showNumberDec(VALIDATION_DATA['N'/015]);
+ delay(DISPLAY_DELAY);
 
- axkh_wblietr.lahpGnfuxkWxv(OTEBWTMBHG_WTMT['J'/9u0990]);
- wxetr(WBLIETR_WXETR);
+ hero_display.showNumberDec(VALIDATION_DATA['Q'/0b1001]);
+ delay(DISPLAY_DELAY);
 
- axkh_wblietr.lahpGnfuxkWxv(OTEBWTMBHG_WTMT[959-'$']);
- wxetr(WBLIETR_WXETR);
+ hero_display.showNumberDec(VALIDATION_DATA[060-'$']);
+ delay(DISPLAY_DELAY);
 }
+
 
