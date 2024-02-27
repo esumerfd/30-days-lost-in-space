c814c6b6e7dec66a1cfb96e77e9465ce0bfa2f08
Edward Sumerfield <esumerfield@stackct.com>
2024-02-26 20:34:21 -0500

    Complete day 23 - it finished with an abort

--- day-23/launch-system/launch-system.ino
+++ day-23/launch-system/launch-system.ino
@@ -39,34 +39,13 @@ void setup() {
   lander_display.setFontRefHeightText();
   lander_display.setFontPosTop();
 
-  /*
-   * Display starting status on our lander's OLED display.
-   *
-   * The U8g2 library for Arduino uses a technique called "page buffering" to draw on
-   * displays connected to microcontrollers with limited RAM. Here's a short explanation
-   * of how it works:
-   *
-   * firstPage(): This function starts the drawing routine. It initializes the buffer with
-   *              the first "page" or segment of the display.
-   * nextPage(): After drawing the first page, this function is called in a loop. Each call
-   *             loads the next segment into the buffer and allows you to draw additional
-   *             content. This process repeats until the entire display content is drawn over
-   *             multiple pages.
-   * Looping: The combination of firstPage() and nextPage() is typically used inside a loop.
-   *          The loop continues until nextPage() returns false, indicating that all pages
-   *          have been drawn.
-   *
-   * By drawing the display in segments, the library conserves RAM, making it feasible to use
-   * graphics displays with microcontrollers that have limited memory resources.
-   */
   lander_display.firstPage();
+
   do {
     byte y_offset = drawString(0, 0, "Exploration Lander");
     drawString(0, y_offset, "Liftoff Sequence");
 
-    // Status on bottom line of OLED display
     drawString(0, lander_display.getDisplayHeight() - lander_display.getMaxCharHeight(), "Countdown Active");
-    // Draw a picture of our lander int bottom right corner
     displayLander(lander_display.getDisplayWidth() - LANDER_WIDTH,
                   lander_display.getDisplayHeight() - LANDER_HEIGHT);
   } while (lander_display.nextPage());
@@ -83,48 +62,35 @@ void setup() {
 
 // *********************************************
 void loop() {
-  // Initialize static variables first time through loop() that will retain values
-  // between loop() executions.
   static unsigned long timeRemaining = COUNTDOWN_MILLISECONDS;
   static unsigned long countdown_start_time = millis();
 
-  Serial.println(timeRemaining);  // Display milliseconds remaining on serial console
-  displayCounter(timeRemaining);  // Display minutes:seconds on counter display
+  Serial.println(timeRemaining);
+  displayCounter(timeRemaining);
 
-  // If timeRemaining has reached 0 display ending values
   if (timeRemaining == 0) {
-    Serial.println("Done!!");           // indicate completion on serial console
-    counter_display.setSegments(DONE);  // "dOnE" on our counter
+    Serial.println("Done!!");
+    counter_display.setSegments(DONE);
 
-    // Now update our OLED display with ending screen using firstPage()/nextPage()
     lander_display.firstPage();
     do {
-      // Each time we display a line of text on our display the y_offset
-      // is updated to point to the next available point for drawing.
-      // Display first two lines
       byte y_offset = drawString(0, 0, "Exploration Lander");
       y_offset = drawString(0, y_offset, "Liftoff ABORTED");
 
-      // Set y_offset to point four lines above bottom of display
       y_offset = lander_display.getDisplayHeight() - (4 * lander_display.getMaxCharHeight());
-      // Display last four lines
       y_offset = drawString(0, y_offset, "Thrusters: OFF");
       y_offset = drawString(0, y_offset, "Systems: OFF");
       y_offset = drawString(0, y_offset, "Confirm: OFF");
       drawString(0, y_offset, "Countdown ABORT");
-      // Draw a picture of our lander in bottom right corner
+
       displayLander(lander_display.getDisplayWidth() - LANDER_WIDTH,
                     lander_display.getDisplayHeight() - LANDER_HEIGHT);
     } while (lander_display.nextPage());
 
-    // Stop here by looping forever.
     while (1)
       ;
   }
 
-  // Update our remaining time by subtracting the start time from current
-  // execution time (in milliseconds).  If our elapsed time is greater
-  // than our countdown time then set remaining time to 0.
   unsigned long elapsed_time = millis() - countdown_start_time;
   if (elapsed_time < COUNTDOWN_MILLISECONDS) {
     timeRemaining = COUNTDOWN_MILLISECONDS - elapsed_time;
@@ -133,27 +99,19 @@ void loop() {
   }
 }
 
-// Display milliseconds as minutes:seconds (MM:SS)
 void displayCounter(unsigned long milliseconds) {
-  // To display the countdown in mm:ss format, separate the parts
   byte minutes = numberOfMinutes(milliseconds);
   byte seconds = numberOfSeconds(milliseconds);
 
-  // Display the minutes in the first two places, with colon
   counter_display.showNumberDecEx(minutes, 0b01000000, true, 2, 0);
-  // This displays the seconds in the last two places
   counter_display.showNumberDecEx(seconds, 0, true, 2, 2);
 }
 
-// Draw test on our lander display at x, y, returning new y
-// value that is immediately below the new line of text.
 byte drawString(byte x, byte y, char *string) {
   lander_display.drawStr(x, y, string);
-  return (y + lander_display.getMaxCharHeight());  // return new y_offset on display
+  return (y + lander_display.getMaxCharHeight());
 }
 
-// Draw an image of our lander drawn with frames and triangles
-// at location x_location, y_location (relative to the upper left corner).
 void displayLander(byte x_location, byte y_location) {
   lander_display.drawFrame(x_location + 7, y_location, 6, 5);        // ship top
   lander_display.drawFrame(x_location + 5, y_location + 4, 10, 20);  // ship center
