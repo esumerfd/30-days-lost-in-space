eee919b86f1520a2109926ad26588c01e9c2a7c1
Edward Sumerfield <esumerfield@stackct.com>
2024-03-02 10:00:56 -0500

    complete day 26

--- day-26/autopilot/autopilot.ino
+++ day-26/autopilot/autopilot.ino
@@ -1,102 +1,14 @@
-/*
- * 30 Days - Lost in Space
- * Day 26 - Fun With Bitmaps
- *
- * Explorer, your lander is finally back in space, but we still need to rejoin
- * the mother ship so more work is required.
- *
- * As we continue to build up your control panel we can see that our loop
- * is running slower and slower, especially as we draw more complex graphics
- * on the display (like our lander).
- *
- * There is a more efficient way to display graphics on our display though.
- * What if we draw an image *once* and save that picture?  Then we can just
- * move it quickly onto our display rather than going through the work to draw
- * each piece separately.
- *
- * Our graphics library provides a way to do JUST that using something called
- * a "bitmap".  This is simply a representation of an image using zero and one
- * bits.  A zero bit leaves a pixel turned off, and a one bit turns it on.
- *
- * Our image can now be saved in our sketch as an array of bits that then gets
- * mapped to our display.  (Thus, "bitmaps").  You can find a number of sample
- * images below our loop() below, along with an explanation of how they were
- * created and represented in our sketch.
- *
- * Learn more at https://inventr.io/adventure
- *
- * Alex Eschenauer
- * David Schmidt
- * Greg Lyzenga
- */
-
-/*
- * Arduino concepts introduced/documented in this lesson.
- * - bitmaps
- * - hexadecimal
- * - forward declarations
- * - pointers
- *
- * Parts and electronics concepts introduced in this lesson.
- * - 
- */
-
-// Explicitly include Arduino.h
 #include "Arduino.h"
+#include <U8g2lib.h>  
+#include "Wire.h"     
 
-// Extensive documentation for this library can be found at https://github.com/olikraus/u8g2
-#include <U8g2lib.h>  // Include file for the U8g2 library.
-#include "Wire.h"     // Sometimes required for I2C communications.
-
-// Use _2_ version of constructor and firstPage()/nextPage() for OLED
-// graphics to save memory.
 U8G2_SH1106_128X64_NONAME_2_HW_I2C lander_display(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
 
-/*
- * Forward declarations
- *
- * Our bitmaps need to be declared in our sketch before they are referenced
- * by code.  But the declaration for each of these images can extend for many
- * lines, forcing us to scroll down to find our setup(), loop() and any
- * additional functions.
- *
- * In order to keep our code near the top of our file we can use a "forward
- * declaration".  This starts with "extern" which tells the compiler that
- * the remaining data is "external" to this line.  The remainder of the line
- * gives enough information about the name and structure of the data so that
- * code can be created.
- *
- * The full declaration MUST be included in our sketch, but now we can move
- * those long declaration to the BOTTOM of our sketch so our setup() and loop()
- * are easy to find near the top of our sketch.
- */
 extern const unsigned char niceRocketBro[] U8X8_PROGMEM;
 extern const unsigned char planetBitmap[] U8X8_PROGMEM;
 extern const unsigned char earthBitmap[] U8X8_PROGMEM;
 extern const unsigned char inventrCorpLogoBitmap[] U8X8_PROGMEM;
 
-/*
- * Data pointers (addresses)
- *
- * In this sketch we wish to display each of the images in turn
- * on our lander display.  We COULD write this like:
- *
- * lander_display.drawXBM(...); // Draw first image
- * delay(2000);                 // delay 2 seconds
- * lander_display.drawXBM(...); // Draw second image
- * delay(2000);                 // delay 2 seconds
- *
- * But this is very repetative and wordy.  If we simply had a list
- * of images in an array we could then write a loop that displays
- * each in turn.
- *
- * Arduino C++ allows us to do this, using something called a "pointer".
- * A pointer represents the ADDRESS in memory of the first byte of
- * a piece of data (like the arrays of data for our images).
- *
- * This array simply holds pointers to each of our image arrays and
- * we can now reference each images using bitmaps[0], bitmap[1], etc.
- */
 const static char* bitmaps[] = {
   niceRocketBro,
   planetBitmap,
@@ -104,58 +16,24 @@ const static char* bitmaps[] = {
   inventrCorpLogoBitmap,
 };
 
-// ************************************************
 void setup(void) {
   Serial.begin(9600);
-  lander_display.begin();   // initialize lander display
+  lander_display.begin();
 }
 
-// ************************************************
 void loop(void) {
 
-  // The number of items in our array is the total size of our array (in bytes)
-  // divided by the size of the first item.
   int number_of_bitmaps = sizeof(bitmaps) / sizeof(bitmaps[0]);
   
-  // Now display each of the bitmaps followed by a delay.
   for (int i = 0; i < number_of_bitmaps; i++) {
     lander_display.firstPage();
     do {
-      // .drawXBMP() displays each bitmap starting at the upper left corner
-      // (0, 0) that has width of 128 and height of 64.
       lander_display.drawXBMP(0, 0, 128, 64, bitmaps[i]);
     } while (lander_display.nextPage());
 
-    // Display each bitmap for 2 seconds.
     delay(2000);
   }
 }
-
-/*
- * Graphics data arrays definition
- *
- * We need to define each of our images using zeros and ones and there are a
- * number of ways we can do this.
- *
- * 1) We could use decimal numbers to represent each byte of data using the
- *    numbers 0-255:
- *      234, 0, 127, 255, ...
- *    But this is hard to align since each byte is 1-3 digits long.
- * 2) We could use binary constants like you've used before:
- *      0b11101101, 0b00000000, 0b01111111, 0b11111111, ...
- *    But this isn't very compact and is more error prone if typed manually.
- *
- * There is another way of representing data that is commonly used for computer
- * code.  Instead of only having 0 or 1 (two values) to represent a single bit
- * we can use more symbol.  We could use decimal (base 10) numbers but this doesn't
- * map well onto computers with data lengths that are powers of two (1, 2, 4, 8, etc.).
- *
- * Programmers created Hexadecimal notation to address these issues.  Hexadecimal
- * is a base-16 number system, meaning it has 16 distinct symbols: 0-9 to represent
- * values zero to nine, and A-F to represent values ten to fifteen.  This allows
- * data to be represented in a compact format that aligns well with bit lengths that
- * are powers of 2.
- */
 
 static const unsigned char niceRocketBro[] U8X8_PROGMEM = {
   0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
